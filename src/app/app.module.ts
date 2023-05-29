3d36b145a00e22fc9d8dec92fd062e25a7b0e170
taiaz <trantantai20081997@gmail.com>
2023-05-29 13:55:34 +0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+// import {FlexModule} from '@angular/flex-layout/flex';
+// import {GridModule} from '@angular/flex-layout/grid';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
